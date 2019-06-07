48be390300b920eb80fe846d73cdd2de19ee49bb
Nikhil <nikhil.rajendrakumar@gmail.com>
2019-06-07 14:49:41 -0400

    Final Update for pairplots iris

--- Reach_Updated.py
+++ Reach_Updated.py
@@ -5,11 +5,15 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-from sklearn.datasets import load_boston
-boston = load_boston()
-bos = pd.DataFrame(boston.data)
-bos.columns = boston.feature_names
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn import datasets
+import seaborn as sns
 
-correlation_matrix = bos.corr().round(2)
-sns.heatmap(data=correlation_matrix, annot=True)
+iris = sns.load_dataset("iris")
+g = sns.pairplot(iris)
+g = sns.pairplot(iris, hue="species", markers=["o", "s", "D"])
+g = sns.pairplot(iris, kind="reg")
+g = sns.pairplot(iris, kind="reg", hue="species", diag_kind='kde')
 plt.show()
